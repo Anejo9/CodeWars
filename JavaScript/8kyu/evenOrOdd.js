c10eb9303fe7d34c250ad652421069d6c790373a
Alejandro <166558483+Anejo9@users.noreply.github.com>
2024-08-19 16:40:19 -0700

    Returns a integer as Even or Odd
    29th exercise

---
+++ JavaScript/8kyu/evenOrOdd.js
@@ -0,0 +1,13 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+// My Solution 
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) {
+//     return 'Even'
+//   }else{
+//     return 'Odd'
+//   }
+// }
+
+const evenOrOdd = (number) => number % 2 ? 'Odd' : 'Even';
