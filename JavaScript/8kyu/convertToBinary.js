5079b5325b2d47e6a60c562866072bf0f3fbbece
Alejandro <166558483+Anejo9@users.noreply.github.com>
2024-09-13 10:23:43 -0700

    Converts given number into binary fromat
    72nd exercise

---
+++ JavaScript/8kyu/convertToBinary.js
@@ -0,0 +1,12 @@
+// Given a non-negative integer n, write a function which returns that number in a binary format as an integer.
+
+// Examples:
+// n = 1 -> 1
+// n = 5 -> 101
+// n = 11 -> 1011
+
+
+// My Solution 
+function toBinary(n){
+    return + n.toString(2)
+  }
