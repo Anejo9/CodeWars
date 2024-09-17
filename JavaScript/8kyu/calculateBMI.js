94b29d088af56daada384538e4edb8e19c2c6d9f
Alejandro <166558483+Anejo9@users.noreply.github.com>
2024-09-17 10:08:08 -0700

    Determines that staus of your BMI
    78th exercise

---
+++ JavaScript/8kyu/calculateBMI.js
@@ -0,0 +1,31 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+
+// My Solution 
+const bmi = (w,h,b = w / h ** 2) => 
+    b <= 18.5 ? 'Underweight' :
+    b <= 25.0 ? 'Normal' :
+    b <= 30 ? 'Overweight' : 'Obese'
+  
+  
+  // function bmi(weight, height) {
+  //   let bmi = weight / height ** 2
+  //   if (bmi <= 18.5) {
+  //     return 'Underweight'
+  //   } else if (bmi <= 25.0) {
+  //     return 'Normal'
+  //   } else if (bmi <= 30.0) {
+  //     return 'Overweight'
+  //   } else if (bmi <= 30) {
+  //     return 'Obese'
+  //   }
+  // }
